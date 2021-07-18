88118cfa562f3c8c0bb4893f629f43b3cda4a4d0
valebladi <13800916+valebladi@users.noreply.github.com>
2021-07-18 22:27:53 +0200

    Update weapon.cs

--- Assets/scripts/weapon.cs
+++ Assets/scripts/weapon.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
 
 public class weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bullet;
-    //bool facing = false;
-    // Start is called before the first frame update
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, System.Action> actions = new Dictionary<string, System.Action>();
+   
     void Start()
     {
-        
+       actions.Add("shoot",Shoot);
+       keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+       keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+       keywordRecognizer.Start();
+    }
+
+    
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log("Voice: "+speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, firePoint.position,firePoint.rotation);
     }
 
     // Update is called once per frame
@@ -19,10 +38,6 @@ public class weapon : MonoBehaviour
         if (Input.GetButtonDown("Fire1")){
             Shoot();
         }
-    }
-
-    void Shoot(){
-        Instantiate(bullet, firePoint.position,firePoint.rotation);
     }
 
     
